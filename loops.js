d00be009db17312ed1e451b29601fb596f81d29a
Millie <milliealexblack@gmail.com>
2019-07-09 01:22:10 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,10 @@
+// function forLoop(array) {
+//   for (let i = 0; i < 25; i++) {
+//     array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`);
+//   }
+//   return array;
+// }
+
+functuon forLoop() {
+  
+}
